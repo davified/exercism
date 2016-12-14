df0059a294f65da00177a16429797cdda9c470b2
David Tan <davified@gmail.com>
2016-12-14 17:07:28 +0800

    added template solution

--- java/nucleotide-count/src/main/java/DNA.java
+++ java/nucleotide-count/src/main/java/DNA.java
@@ -1,22 +1,41 @@
-public class DNA {
-    private String dnaString;
-    private char[] dnaArray;
-    private int counter;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collections;
 
-    public DNA(String string) {
-        this.dnaString = string;
-        this.dnaArray = dnaString.toCharArray();
+public class DNA {
+    private final Map<Character, Integer> nucleotideMap = new HashMap<>();
+    private final String dnaSequence;
+
+    public DNA(String dnaSequence) {
+        this.dnaSequence = dnaSequence;
+        initializeDefaultNucleotideMap();
+        if (!dnaSequence.isEmpty()) {
+            countNucleotides();
+        }
     }
 
-    public int count(Character input) {
-        int counter = 0;
+    private Map<Character, Integer> initializeDefaultNucleotideMap() {
+        nucleotideMap.put('A', 0);
+        nucleotideMap.put('C', 0);
+        nucleotideMap.put('G', 0);
+        nucleotideMap.put('T', 0);
+        return nucleotideMap;
+    }
 
-        for (int i = 0; i < dnaArray.length ; i++) {
-            if (dnaArray[i] == input) {
-                counter++;
-            }
+    private void countNucleotides() {
+        for (Character chr : dnaSequence.toCharArray()) {
+            nucleotideMap.put(chr, nucleotideMap.get(chr) + 1);
         }
+    }
 
-        return counter;
+    public int count(char nucleotide) {
+        if (!nucleotideMap.containsKey(nucleotide)) {
+            throw new IllegalArgumentException("Invalid nucleotide");
+        }
+        return nucleotideMap.get(nucleotide);
+    }
+
+    public Map<Character, Integer> nucleotideCounts() {
+        return Collections.unmodifiableMap(nucleotideMap);
     }
 }
